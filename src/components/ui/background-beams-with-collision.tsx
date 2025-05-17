3df774c23c3f1a24f88a68d5d142cb24f20fe494
Raihan Musthafa Kamal <114491951+raihanrpl@users.noreply.github.com>
2025-05-18 01:54:51 +0800

    update posisi text di hero section

--- src/components/ui/background-beams-with-collision.tsx
+++ src/components/ui/background-beams-with-collision.tsx
@@ -70,7 +70,7 @@ export const BackgroundBeamsWithCollision = ({
     <div
       ref={parentRef}
       className={cn(
-        "h-[25rem] md:h-[20rem] relative flex items-center w-full justify-center overflow-hidden",
+        "h-[22rem] md:h-[20rem] relative flex items-center w-full justify-center overflow-hidden",
         className
       )}
     >
--- src/page/HeroSection.tsx
+++ src/page/HeroSection.tsx
@@ -8,12 +8,12 @@ export function HeroSection() {
   return (
     
       <section className="relative h-[100vh] items-center justify-center overflow-hidden">
-  <BackgroundBeamsWithCollision className="relative  z-0">
+  <BackgroundBeamsWithCollision className="relative z-0">
     <Spotlight className="absolute inset-0 z-0 opacity-20" />
   </BackgroundBeamsWithCollision>
 
 <div
-  className="relative z-20 text-center -translate-y-6"
+  className="relative z-20 text-center -translate-y-3 md:-translate-y-6"
 >
     <h1 className="text-4xl md:text-6xl font-bold text-emerald-800">
       Raihan Musthafa Kamal
